f3904823d84589c57a2674739eba05583734fbc3
Diogo Cardoso <diogomescardoso@gmail.com>
2021-09-23 23:26:06 -0300

    correção

---
+++ src/main/java/br/edu/ifpb/pweb2/sortetudo/config/AppConfiguration.java
@@ -0,0 +1,39 @@
+package br.edu.ifpb.pweb2.sortetudo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.IEngineConfiguration;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+
+import java.util.Map;
+
+@EnableWebMvc
+@ComponentScan(basePackages = "br.edu.ifpb.pweb2.sortetudo.controller")
+public class AppConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver(){
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        //templateEngine.setTemplateResolver();
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+}
